9627c3056b8a98c23f2200b53c45c84314cede63
Alex Foglia <alex.f1793@gmaill.com>
2022-03-28 01:21:55 +0100

    Firmware updated

--- src/firmware/firmware.ino
+++ src/firmware/firmware.ino
@@ -1,15 +1,13 @@
-// the setup routine runs once when you press reset:
-void setup() {
-  // initialize serial communication at 9600 bits per second:
+void setup()
+{
   Serial.begin(9600);
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  // read the input on analog pin 0:
+
+void loop()
+{
   int sensorValue = analogRead(A0);
   float vInPercentage = (float)sensorValue / 1023.f;
 
-  // print out the value you read:
   Serial.println(vInPercentage);
 }
