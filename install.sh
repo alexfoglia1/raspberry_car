64cd97b819813c1fca7ff66a0581044b02eeeb7c
Alex Foglia <alex.f1793@gmaill.com>
2022-03-28 03:48:51 +0100

    01.01-R : Installer script

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+echo "**** INSTALLING RASPBERRY CAR ****"
+echo ""
+echo "Compiling..."
+echo ""
+mkdir build
+cd build
+cmake ..
+make all
+echo "127.0.0.1" > settings.ini
+cd ../src/scripts/
+
+echo ""
+echo "Installing services..."
+echo ""
+
+webserv=/etc/systemd/system/web-service.service
+imuserv=/etc/systemd/system/imu-service.service
+carserv=/etc/systemd/system/raspberry-car.service
+
+if test -f "$webserv"; then
+    echo "$webserv exists: removing old web service installation"
+    sudo systemctl disable web-service
+    sudo rm -fr "$webserv"
+fi
+
+if test -f "$imuserv"; then
+    sudo systemctl disable imu-service
+    echo "$imuserv exists: removing old imu service installation"
+    sudo rm -fr "$imuserv"
+fi
+
+
+if test -f "$carserv"; then
+    sudo systemctl disable raspberry-car
+    echo "$carserv exists: removing old raspberry-car service installation"
+    sudo rm -fr "$carserv"
+fi
+
+sudo sh install-services.sh
+
+cd ../../
+
+echo "**** DONE ****"
+echo ""
+echo ""
+echo "You need to know the controller address."
+echo "Type it inside build/settings.ini"
+echo "Or change it via the web service."
+echo ""
+echo ""
+echo "To run the application, type:"
+echo "sudo systemctl start imu-service"
+echo "sudo systemctl start web-service"
+echo "sudo systemctl start raspberry-car"
+echo "or reboot the system."
